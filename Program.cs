4933afd2e27cc5ea693a3cf9502a7714a7818d5b
jconvencido <110988762+jconvencido@users.noreply.github.com>
2022-08-18 17:30:48 +0800

    initial Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstProgramHRM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = ClosedPath(8888);
+            Console.ReadLine();
+        }
+
+        static int ClosedPath(int number)
+        {
+            int ctr = 0;
+            int[] closedPath = {1,0,0,0,1,0,1,0,2,1};
+            int currentNumber;
+
+            while (number > 0)
+            {
+                currentNumber = number % 10;
+
+                ctr += closedPath[currentNumber];
+
+                number /= 10;
+            }
+            return ctr;
+        }
+    }
+}
