cb4419cc9b1247aef62f4bcbd6a93f01bce4ce69
jconvencido <110988762+jconvencido@users.noreply.github.com>
2022-08-18 19:03:29 +0800

    Upto module 5

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace FirstProgramHRM
 {
@@ -7,9 +8,63 @@ namespace FirstProgramHRM
     {
         static void Main(string[] args)
         {
-            var i = ClosedPath(8888);
-            Console.ReadLine();
+            UsingRefParameters();
+            UsingParameters();
         }
+
+        private static void UsingRefParameters()
+        {
+            int monthlyWage1 = 1234;
+            int monthlyWage2 = 2000;
+            int months1 = 12;
+            int months2 = 8;
+            int bonus = 300;
+
+            var yearlyWage1 = CalculateYearlyWagebyRef(monthlyWage1, months1, ref bonus);
+            Console.WriteLine(yearlyWage1);
+
+            var yearlyWage2 = CalculateYearlyWage(monthlyWage2, months2, bonus);
+            Console.WriteLine(yearlyWage2);
+            //Console.WriteLine(ClosedPath(yearlyWage));
+        }
+        private static void UsingParameters()
+        {
+            int monthlyWage1 = 1234;
+            int monthlyWage2 = 2000;
+            int months1 = 12;
+            int months2 = 8;
+            int bonus = 300;
+
+            var yearlyWage1 = CalculateYearlyWage(monthlyWage1, months1, bonus);
+            Console.WriteLine(yearlyWage1);
+
+            var yearlyWage2 = CalculateYearlyWage(monthlyWage2, months2, bonus);
+            Console.WriteLine(yearlyWage2);
+            //Console.WriteLine(ClosedPath(yearlyWage));
+        }
+
+        public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked, int bonus)
+        {
+            if (monthlyWage >= 2000)
+            {
+                bonus *= 2;
+            }
+            Console.WriteLine($"Bonus {bonus}");
+            return monthlyWage * numberOfMonthsWorked + bonus;
+        }
+        public static int CalculateYearlyWagebyRef(int monthlyWage, int numberOfMonthsWorked,ref int bonus)
+        {
+            if (monthlyWage > 2000)
+            {
+                bonus *= 2;
+            }
+            Console.WriteLine($"Bonus {bonus}");
+            return monthlyWage * numberOfMonthsWorked + bonus;
+        }
+
+
+
+
 
         static int ClosedPath(int number)
         {
